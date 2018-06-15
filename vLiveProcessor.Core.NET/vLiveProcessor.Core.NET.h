b034e2cbeb272b0a72c5363fff2f401e2352f794
rozandoz <powerstatus524@gmail.com>
2018-06-15 15:20:09 +0200

    Add empty project for .NET library

---
+++ vLiveProcessor.Core.NET/vLiveProcessor.Core.NET.h
@@ -0,0 +1,13 @@
+// vLiveProcessor.Core.NET.h
+
+#pragma once
+
+using namespace System;
+
+namespace vLiveProcessorCoreNET {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
